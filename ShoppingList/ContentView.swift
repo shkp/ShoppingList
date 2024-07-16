0804695a13e3e078fabaadcb3d9dd77261afb182
Sumukha Kashyap <sumukhakashyap@gmail.com>
2024-07-16 23:06:34 +0200

    Initial commit

--- ShoppingList/ContentView.swift
+++ ShoppingList/ContentView.swift
@@ -8,17 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedItems: [Bool] = Array(repeating: false, count: 100)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (alignment: .leading){
+            
+            Text("Shopping List")
+                .font(.system(size: 38, weight: .semibold))
+                .foregroundColor(.black)
+            
         }
+        .frame(maxWidth: .infinity, maxHeight: 40, alignment: .topLeading)
         .padding()
+        
+        Spacer()
+        
+        List{
+            ForEach(0..<100, id: \.self){ index in
+                HStack{
+                    Circle()
+                        .fill(self.selectedItems[index] ? Color.blue :Color.clear)
+                        .stroke(Color.black, lineWidth:2)
+                        .frame(width: 20)
+                    
+                    Text("Item \(index)")
+                    
+                }
+                .onTapGesture {
+                    self.selectedItems[index].toggle()
+                }
+            }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+            
+        }.listStyle(.plain)
+        
+        
+        
+        
+        
     }
+    
 }
-
+    
 #Preview {
-    ContentView()
+        ContentView()
 }
+        
+        
+        
+    
