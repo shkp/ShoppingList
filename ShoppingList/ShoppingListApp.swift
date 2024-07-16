520f9f0b760fdda21babb6eefe60396150d3bb83
Sumukha Kashyap <sumukhakashyap@gmail.com>
2024-07-16 19:59:53 +0200

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Sumukha Kashyap on 16.07.24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
